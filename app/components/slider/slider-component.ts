afa32d97bc8bb89d65cb0b30102cd90cb2fd2d3c
everardo sanchez <everardo.ipn@gmail.com>
2016-12-05 16:01:31 -0600

    feat: add Slider functionality

--- app/components/slider/slider-component.ts
+++ app/components/slider/slider-component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Input } from '@angular/core';
 
 @Component({
   selector: 'slider-component',
@@ -6,4 +6,80 @@ import { Component } from '@angular/core';
   styleUrls: ['app/components/slider/slider-component.css']
 })
 
-export class SliderComponent {};
+export class SliderComponent {
+  @Input('speed') speed: string;
+  reel :any;
+  constructor(
+    private element: ElementRef
+  ){
+
+  }
+
+  ngOnInit() {
+    if(this.speed === undefined) {
+      this.speed = "50";
+    }
+  }
+
+  ngAfterViewInit() {
+    let _self = this;
+    this.initSlider();
+    this.moveSlider();
+  }
+
+  initSlider() {
+    this.reel = this.element.nativeElement.querySelector("#reel");
+
+    this.cloneFirst();
+    this.reel.style.width = (this.reel.children.length * window.innerWidth) +"px";
+  }
+
+  moveSlider() {
+    let _self = this;
+    let firstImage = this.element.nativeElement.querySelector("img");
+    let difference = firstImage.getBoundingClientRect().left - (window.innerWidth*-2);
+
+    if ( difference < 0 ) {
+      this.reel.removeChild(firstImage);
+      this.cloneFirst(difference);
+    }
+    this.triggerMovement();
+    setTimeout(function() {
+      requestAnimationFrame(_self.moveSlider.bind(_self));
+    }, 500 );
+  }
+
+  triggerMovement() {
+    let allImages = this.element.nativeElement.querySelectorAll("img");
+    for(let i = 0; i < allImages.length; i++ ){
+      let actual = this.getActualTranslate(allImages[i]);
+      let newAdvance = parseInt(this.speed) + actual;
+
+      allImages[i].style.transform = "translate3d(-"+newAdvance+"px,0,0)";
+    }
+
+  }
+
+  removeFirst() {
+
+  }
+
+  cloneFirst(dif: any = 0){
+    let container = this.reel;
+    let firstImage = this.element.nativeElement.querySelector("img");
+    let cloned = firstImage.cloneNode(false);
+
+    cloned.style.left = (window.innerWidth *2 + dif + 20)  + "px";
+    cloned.style.transform ="";
+    container.appendChild(cloned);
+  }
+
+  getActualTranslate(img: any) {
+    let transformAttr = img.style.transform;
+    if (transformAttr == "") {
+      return 0;
+    }
+    let transformX = transformAttr.substring( transformAttr.search("-")+1, transformAttr.search("px,"));
+    return parseInt(transformX);
+  }
+};
