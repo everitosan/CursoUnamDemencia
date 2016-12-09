64ce1f47c0d61dcafa2c98fa0997c01b240dd440
everardo sanchez <everardo.ipn@gmail.com>
2016-12-08 21:43:24 -0600

    feat add vote component

---
+++ app/components/video/video.component.ts
@@ -0,0 +1,15 @@
+import {Component, Input} from '@angular/core';
+
+@Component({
+  selector: "video-view",
+  templateUrl: "app/components/video/video.html",
+  styleUrls: ["app/components/video/video.css"]
+})
+
+export class VideoComponent{
+  @Input('src') src: string;
+
+  ngOnInit(){
+    console.log(this.src);
+  }
+};
